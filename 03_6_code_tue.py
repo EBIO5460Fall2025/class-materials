2da0c0681ec87c07229ce62ec3f5b6c6dad2f8b9
Brett Melbourne <brett.melbourne@colorado.edu>
2025-09-05 18:41:06 -0600

    Add code for week 3 Tue

---
+++ 03_6_code_tue.py
@@ -0,0 +1,12 @@
+# Problem
+
+# A population starts with 2 individuals. Each generation, it doubles in size.
+# What is the population size the first time the population exceeds 1000
+# individuals?
+
+N = 2
+
+while N <= 1000:
+    N = N * 2
+
+print(N)
